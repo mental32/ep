2c2f83faf4dc0f0f5302cbafacab897461d2ddd0
mental32 <m3nta1@yahoo.com>
2018-08-20 09:17:36 +0100

    Made `pep` dynamic in invokation

--- src/cogs/auto.py
+++ src/cogs/auto.py
@@ -2,7 +2,21 @@ import asyncio
 
 from src.utils import GuildCog
 
+_PEP = lambda n: f'https://www.python.org/dev/peps/pep-{str(n).zfill(4)}/'
+
+
 class Automation(GuildCog):
+	async def on_message(self, message):
+		if message.content[:3] == 'PEP':
+			links = []
+
+			for pep in message.content[3:].split():
+				if not pep.isdigit():
+					break
+				links.append(_PEP(int(pep)))
+
+			return await message.channel.send('\n'.join(output))
+
     async def on_member_join(self, member):
         if member.bot:
             return await member.add_roles(self._bot_role)
