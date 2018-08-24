ce7e80a83c05bcec701ab49acc43988d9eb05127
mental32 <m3nta1@yahoo.com>
2018-08-24 07:19:00 +0100

    Began the webserver admin panel

--- src/cogs/site/blueprints/index.py
+++ src/cogs/site/blueprints/index.py
@@ -5,4 +5,8 @@ blueprint = flask.Blueprint(__name__, 'index')
 @blueprint.route('/')
 @blueprint.route('/index')
 def index():
-	return 'Hello world'
+	return flask.render_template('index.html', bot=flask.current_app.bot)
+
+@blueprint.route('/profile/<int:guild_id>')
+def test_profile(guild_id):
+	return f'{flask.escape(repr(flask.current_app.bot.get_guild(guild_id)))}'
--- src/cogs/site/core.py
+++ src/cogs/site/core.py
@@ -9,10 +9,15 @@ import flask
 from src.utils import GuildCog
 from src.core import _LIB_PATH
 
-_FLASK_BLUEPRINTS = _LIB_PATH.joinpath('cogs', 'site', 'blueprints')
+_SITE_PATH = _LIB_PATH.joinpath('cogs', 'site')
+_FLASK_BLUEPRINTS = _SITE_PATH.joinpath('blueprints')
+
+_TEMPLATE_PATH = _SITE_PATH.joinpath('templates').absolute()
+_STATIC_PATH = _SITE_PATH.joinpath('static').absolute()
 
 def _cast_py_import_path(path):
     return str(path).replace('\\', '.').replace('/', '.')
+
 
 class Site(GuildCog):
     def __init__(self, bot):
@@ -22,7 +27,8 @@ class Site(GuildCog):
         logger.setLevel(logging.INFO)
         logger.addHandler(logging.FileHandler('ep_app.log', 'a+'))
 
-        self._app = flask.Flask(__name__)
+        self._app = flask.Flask(__name__, template_folder=str(_TEMPLATE_PATH), static_folder=str(_STATIC_PATH))
+        self._app.bot = bot
 
         for file in os.listdir(f'{_FLASK_BLUEPRINTS}'):
             name = file.rsplit('.', maxsplit=1)[0]
