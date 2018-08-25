6b66bd0c1f76142efd5422118a7e955a95f19ebc
mental32 <m3nta1@yahoo.com>
2018-08-25 07:13:55 +0100

    changed indentation to spaces

--- install.py
+++ install.py
@@ -8,53 +8,53 @@ import importlib
 
 PYTHON_PATH = sys.executable
 DEPENDENCIES = [
-	{
-		'name': 'discord', 
-		'src': 'https://github.com/Rapptz/discord.py@rewrite#egg=discord.py[voice]',
-		'git': True,
-		'check': lambda m: (m.__version__, m.__author__) == ('1.0.0a', 'Rapptz')
-	},
-	{
-		'name': 'flask',
-	}
+    {
+        'name': 'discord', 
+        'src': 'https://github.com/Rapptz/discord.py@rewrite#egg=discord.py[voice]',
+        'git': True,
+        'check': lambda m: (m.__version__, m.__author__) == ('1.0.0a', 'Rapptz')
+    },
+    {
+        'name': 'flask',
+    }
 ]
 
 
 def install_dependency(name, git=False, src=None, check=None):
-	check = check or (lambda key: True)
+    check = check or (lambda key: True)
 
-	def dependency_check(name, pred, exception=ImportError):
-		try:
-			module = importlib.import_module(name)
-			if not pred(module):
-				raise ImportError
-			version = module.__version__
-		finally:
-			try:
-				del module
-				del sys.modules[name]
-			except NameError:
-				pass
+    def dependency_check(name, pred, exception=ImportError):
+        try:
+            module = importlib.import_module(name)
+            if not pred(module):
+                raise ImportError
+            version = module.__version__
+        finally:
+            try:
+                del module
+                del sys.modules[name]
+            except NameError:
+                pass
 
-		return version
+        return version
 
-	try:
-		version = dependency_check(name, check)
-	except ImportError:
-		status = subprocess.check_call(f'{PYTHON_PATH} -m pip install -U{" git+" if git else " "}{name if src is None else src}')
-		dependency_check(name, check, exception=RuntimeError(f'fatal: Could not install dependency: {name}[{src}]'))
-		return status, None
-	else:
-		return -1, version
+    try:
+        version = dependency_check(name, check)
+    except ImportError:
+        status = subprocess.check_call(f'{PYTHON_PATH} -m pip install -U{" git+" if git else " "}{name if src is None else src}')
+        dependency_check(name, check, exception=RuntimeError(f'fatal: Could not install dependency: {name}[{src}]'))
+        return status, None
+    else:
+        return -1, version
 
 def main():
-	for dep in DEPENDENCIES:
-		print(f'Installing {dep["name"]}...', end='')
-		sts, _v = install_dependency(**dep)
-		if sts == -1:
-			print(f'Already installed: {dep["name"]}({_v})')
-		else:
-			print(f'Installed {dep["name"]}')
+    for dep in DEPENDENCIES:
+        print(f'Installing {dep["name"]}...', end='')
+        sts, _v = install_dependency(**dep)
+        if sts == -1:
+            print(f'Already installed: {dep["name"]}({_v})')
+        else:
+            print(f'Installed {dep["name"]}')
 
 if __name__ == '__main__':
-	main()
+    main()
