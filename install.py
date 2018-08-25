fbb14659f005a6ead0934cba14533a688a763985
mental32 <m3nta1@yahoo.com>
2018-08-25 06:48:11 +0100

    Add install script

---
+++ install.py
@@ -0,0 +1,55 @@
+import sys
+
+assert sys.version_info[:2] >= (3, 6), 'fatal: requires python3.6+'
+
+import argparse
+import subprocess
+import importlib
+
+PYTHON_PATH = sys.executable
+DEPENDENCIES = [
+	{
+		'name': 'discord', 
+		'src': 'https://github.com/Rapptz/discord.py@rewrite#egg=discord.py[voice]',
+		'git': True,
+		'check': lambda m: (m.__version__, m.__author__) == ('1.0.0a', 'Rapptz')
+	},
+	{
+		'name': 'flask',
+	}
+]
+
+
+def install_dependency(name, git=False, src=None, check=None):
+	check = check or (lambda key: True)
+
+	def dependency_check(name, pred, exception=ImportError):
+		try:
+			module = importlib.import_module(name)
+			if not pred(module):
+				raise ImportError
+			return module.__version__
+		finally:
+			del module
+			del sys.modules[name]
+
+	try:
+		version = dependency_check(name, check)
+	except ImportError:
+		status = subprocess.check_call(f'{PYTHON_PATH} -m pip install -U{" git+" if git else " "}{name if src is None else src}')
+		dependency_check(name, check, exception=RuntimeError(f'fatal: Could not install dependency: {name}[{src}]'))
+		return status, None
+	else:
+		return -1, version
+
+def main():
+	for dep in DEPENDENCIES:
+		print(f'Installing {dep["name"]}...', end='')
+		sts, _v = install_dependency(**dep)
+		if sts == -1:
+			print(f'Already installed: {dep["name"]}({_v})')
+		else:
+			print(f'Installed {dep["name"]}')
+
+if __name__ == '__main__':
+	main()
