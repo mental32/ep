756ef96bbbb2efa3f855afff93bfc2cd7e45b455
mental32 <m3nta1@yahoo.com>
2018-08-27 08:44:18 +0100

    Update profile

--- src/cogs/view.py
+++ src/cogs/view.py
@@ -8,6 +8,7 @@ _remaining_todo = {
     ],
     'Not implemented': [
         'PyDoc'
+        'PyProfile'
         'Python code sandbox',
         'CPython bug/branch tracker',
         'Forward PSF tweets?',
@@ -24,13 +25,16 @@ class General:
     async def test_source(self, ctx):
         return await ctx.send('https://github.com/mental32/ep_bot')
 
-    @commands.command(name='pep')
+    @commands.command(name='PEP')
     async def test_pep(self, ctx, pep_number: int):
         return await ctx.send(f'https://www.python.org/dev/peps/pep-{str(pep_number).zfill(4)}/')
 
     @commands.command(name='profile')
-    async def test_profile(self, ctx, member: discord.Member):
-        pass
+    async def test_profile(self, ctx, member: discord.Member = None):
+        member = member or ctx.author
+        em = discord.Embed(title=f'{member}\'s profile')
+        em.set_thumbnail(url=member.avatar_url)
+        await ctx.send(embed=em)
 
 def setup(bot):
     bot.add_cog(General())
