044ed13f63f6e95a05884a5a3bb12afaa3f59371
mental <m3nta1@yahoo.com>
2019-02-14 00:00:36 +0000

    Remove todo and magic command

--- src/cogs/view.py
+++ src/cogs/view.py
@@ -6,26 +6,7 @@ import discord
 from discord.ext import commands
 
 
-_remaining_todo = {
-    'Complete': [
-        'PyDis'
-    ],
-    'In progress': [
-        'Python code sandbox'
-    ],
-    'Not implemented': [
-        'PyDoc',
-        'CPython bug/branch tracker',
-        'Forward PSF tweets?',
-    ],
-}
-
-
 class General:
-    @commands.command(name='todo')
-    async def test_todo(self, ctx):
-        await ctx.send('\n\n'.join(f'**{key}:**\n' + '\n'.join(f'• {_}' for _ in value) for key, value in _remaining_todo.items() if value))
-
     @commands.command(name='source')
     async def test_source(self, ctx):
         return await ctx.send('https://github.com/mental32/ep_bot')
@@ -33,19 +14,6 @@ class General:
     @commands.command(name='PEP')
     async def test_pep(self, ctx, pep_number: int):
         return await ctx.send(f'https://www.python.org/dev/peps/pep-{str(pep_number).zfill(4)}/')
-
-    @commands.command(name='magic')
-    async def test_magic(self, ctx, number: int = None):
-        try:
-            datapoint = await ctx.bot.db.get(ctx.author.id)
-        except KeyError:
-            datapoint = await ctx.bot.db.set(ctx.author.id, number)
-
-        if number is None:
-            await ctx.send(datapoint.content)
-        else:
-            await datapoint.update(ctx.author.id, number)
-            await ctx.send('\N{OK HAND SIGN}')
 
     @commands.command(name='dis')
     async def test_dis(self, ctx, *, source):
