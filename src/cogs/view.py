8f5f7a6ed63933898ec6ac09c3daf50d07396f2a
mental <m3nta1@yahoo.com>
2019-02-27 19:48:48 +0000

    Update view cog

--- src/cogs/view.py
+++ src/cogs/view.py
@@ -8,15 +8,15 @@ from discord.ext import commands
 
 class General:
     @commands.command(name='source')
-    async def test_source(self, ctx):
+    async def _source(self, ctx):
         return await ctx.send('https://github.com/mental32/ep_bot')
 
     @commands.command(name='PEP')
-    async def test_pep(self, ctx, pep_number: int):
+    async def _pep(self, ctx, pep_number: int):
         return await ctx.send(f'https://www.python.org/dev/peps/pep-{str(pep_number).zfill(4)}/')
 
     @commands.command(name='dis')
-    async def test_dis(self, ctx, *, source):
+    async def _dis(self, ctx, *, source):
         if source.startswith('```py\n'):
             source = source[6:]
 
@@ -27,7 +27,6 @@ class General:
             dis.dis(source)
 
         await ctx.send(f'```py\n{out.getvalue()}```')
-
 
 def setup(bot):
     bot.add_cog(General())
