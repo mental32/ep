d3d47dbe70154fe729a1929a7c7bc4b57696c609
mental <m3nta1@yahoo.com>
2019-02-28 04:29:47 +0000

    Re organise pseudo interface into files

---
+++ src/cogs/pseudo/__init__.py
@@ -0,0 +1,4 @@
+from .core import Pseudo
+
+def setup(bot):
+    bot.add_cog(Pseudo(bot))
---
+++ src/cogs/pseudo/core.py
@@ -0,0 +1,157 @@
+import inspect
+import random
+import string
+import enum
+
+import discord
+from discord.ext.commands import Context
+from discord.ext.commands.view import StringView
+
+from .attrs import allowed
+
+ascii_letters = string.ascii_letters + '_'
+
+
+class TokenTypes(enum.IntEnum):
+    OTHER = 0
+    NAME = 1
+
+
+class Pseudo:
+    def __init__(self, bot):
+        self.bot = bot
+        self._users = {}
+
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        elif len(message.content) >= 2 and message.content.count('`') == 1 and message.content[0] == '`' and message.content[1] in ascii_letters:
+            if message.channel.id != 534364800679936000:
+                return
+
+            try:
+                output = await self._eval(Context(prefix='`', view=StringView(message.content), bot=self.bot, message=message), message.content[1:])
+            except BaseException as error:
+                output = str(error)
+
+            try:
+                await message.channel.send(f'```\n{output}```')
+            except discord.HTTPException as error:
+                await message.channel.send(f'```\n{error}```')
+
+    def _parse(self, source):
+        token = []
+
+        last_bracket = [None]
+        brackets = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+
+        # Check for unbalanced brackets before tokenizing
+        for char in source:
+            if char in brackets:
+                if brackets[char] != last_bracket[-1]:
+                    raise SyntaxError(f'Unmatched bracket: "{char}"')
+                else:
+                    last_bracket.pop()
+
+            elif char in '([{':
+                last_bracket.append(char)
+
+        if last_bracket[-1] is not None:
+            raise SyntaxError(f'Unmatched bracket: "{last_bracket[-1]}"')
+
+        # Begin tokenizing and yielding
+        for char in source:
+            if char not in ascii_letters:
+                if token:
+                    yield ''.join([*token]), TokenTypes.NAME
+                    token = []
+
+                yield char, TokenTypes.OTHER
+
+            else:
+                token += [char]
+
+        if token:
+            yield ''.join([*token]), TokenTypes.NAME
+
+    async def _eval(self, ctx, source):
+        stacks = [[]]
+        stack = stacks[-1]
+
+        scope = {
+            'ctx': ctx,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            'author': ctx.author,
+
+            'choice': lambda seq: random.choice(seq),
+
+            'len': len,
+            'str': str,
+            'repr': repr,
+            'type': type,
+        }
+
+        if ctx.author.id in self._users:
+            scope['_'] = self._users[ctx.author.id]
+
+        check = True
+
+        tokens = self._parse(source)
+
+        for token, type_ in tokens:
+            if type_:
+                if token not in scope:
+                    raise NameError(f'name {token!r} is not defined')
+                else:
+                    stack.append(scope[token])
+
+                continue
+
+            elif token == '.':
+                obj, attr = stack[-1], next(tokens)[0]
+
+                obj_t = type(obj)
+
+                if check and obj_t not in allowed or attr not in allowed[obj_t]:
+                    raise AttributeError(f'\'{obj_t}\' object has no attribute \'{attr}\'')
+
+                value = getattr(obj, attr)
+
+                if check:
+                    func = allowed[obj_t][attr]
+
+                    if func is not None:
+                        value = func(ctx, value)
+
+                        if inspect.iscoroutine(value):
+                            value = await value    
+
+                stack[-1] = value
+
+            elif token == '(':
+                stacks.append([])
+                stack = stacks[-1]
+
+            elif token == ')':
+                value = stacks[-2][-1](*stack)
+
+                if inspect.iscoroutine(value):
+                    value = await value
+
+                stacks.pop()
+                stack = stacks[-1]
+
+                stack[-1] = value
+
+            else:
+                raise SyntaxError(f'Unexpected token! {token!r} (type={type_!r})')
+
+        self._users[ctx.author.id] = stack[-1]
+
+        return stack[-1]
