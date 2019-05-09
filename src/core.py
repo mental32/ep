9bf6385fd34900487e3af56f9dfea02833329dc8
mental <m3nta1@yahoo.com>
2019-05-09 22:54:17 +0100

    Rewrite core

--- src/core.py
+++ src/core.py
@@ -5,27 +5,15 @@ from functools import partial
 
 from discord.ext import commands
 
-import src
-
-_LIB_PATH = pathlib.Path(src.__file__).parents[0]
+_LIB_PATH = pathlib.Path(__file__).parents[0]
 _LIB_EXTS = _LIB_PATH.joinpath('cogs')
+
 _GUILD_SNOWFLAKE = 455072636075245588
-
-
-def snake_case(key):
-    out = list(key.replace('__', '@'))
-
-    while '_' in out:
-        pos = out.index('_')
-        del out[pos]
-        out[pos] = out[pos].upper()
-
-    return ''.join(out).replace('@', '_').title()
 
 
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
-        kwargs['command_prefix'] = ('Py', 'py_')
+        kwargs['command_prefix'] = '?'
 
         super().__init__(*args, **kwargs)
 
@@ -37,38 +25,26 @@ class Bot(commands.Bot):
 
         self.load_extension('jishaku')
 
-        for file in os.listdir(f'{_LIB_EXTS}'):
-            if '__pycache__' in file:
+        for path in _LIB_EXTS.iterdir():
+ 
+            if path.is_dir():
+                name = path.name
+
+            elif path.name.count('.') > 1:
+                print(f'Skipping: {path.name}')
                 continue
 
-            _cut_off = -3
-
-            if _LIB_EXTS.joinpath(file).is_dir():
-                _cut_off = len(file)
-
-            elif file[-3:] != '.py':
-                continue
+            else:
+                name, *_ = path.name.rpartition('.')
 
             try:
-                self.load_extension(f'src.cogs.{file[:_cut_off]}')
+                self.load_extension(f'.cogs.{name}', package=__name__)
             except Exception as err:
-                print(f'Failed to load "{file}" ({err})', file=sys.stderr)
+                print(f'Failed to load "{path.name}" ({err})', file=sys.stderr)
 
     def add_cog(self, klass, *args, **kwargs):
         super().add_cog(klass, *args, **kwargs)
         getattr(klass, f'_{type(klass).__name__}__cog_add', (lambda: None))()
-
-    async def get_context(self, *args, **kwargs):
-        ctx = await super().get_context(*args, **kwargs)
-
-        style = ((lambda key: key) if ctx.prefix != 'Py' else snake_case)
-
-        for cmd, coro in self.all_commands.items():
-            if style(cmd) == ctx.invoked_with:
-                ctx.command = coro
-                break
-
-        return ctx
 
     async def on_connect(self):
         print('Connect...', end='')
