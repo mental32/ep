d98b741f99643a9eded49dea51baf2ea47dc8bc1
mental <m3nta1@yahoo.com>
2019-06-12 00:01:12 +0100

    Add `utils.get_logger`

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,7 +1,12 @@
+import logging
+from typing import Optional
+
+import coloredlogs
+
 from discord import Embed as _Embed
 from discord.ext.commands import Cog as _Cog
 
-__all__ = ('listener', 'event', 'codeblock', 'embed')
+__all__ = ('listener', 'event', 'codeblock', 'embed', 'get_logger')
 
 listener = _Cog.listener
 event = listener()
@@ -15,3 +20,21 @@ def codeblock(string: str, style: str = '') -> str:
 
 def embed() -> _Embed:
     pass
+
+
+def get_logger(
+    name: str, level: str = 'INFO', fmt: Optional[str] = None
+) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(getattr(logging, level))
+
+    fmt = fmt or (
+        '[%(asctime)s] %(levelname)s - %(funcName)s:%(lineno)d - %(module)s - %(message)s'
+    )
+    coloredlogs.install(fmt=fmt, level=level, logger=logger)
+    return logger
+
+
+get_logger(
+    'discord', 'WARN', fmt='[[ discord ]] [%(asctime)s] %(levelname)s - %(message)s'
+)
