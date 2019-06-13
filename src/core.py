370cb299c71fb57fa3484b090e8cffd6b491c760
mental <m3nta1@yahoo.com>
2019-06-14 00:13:15 +0100

    Update core

--- src/core.py
+++ src/core.py
@@ -16,6 +16,9 @@ logger = utils.get_logger(__name__)
 
 
 class Bot(commands.Bot):
+    _guild: discord.Guild = None
+    _cog_counter: int = 0
+
     def __init__(self, *args, **kwargs):
         kwargs['command_prefix'] = ('py_', 'py', 'py?', '?', '>>')
 
@@ -29,31 +32,71 @@ class Bot(commands.Bot):
             )  # Close the underlying http session.
             raise RuntimeError('Could not find `DISCORD_TOKEN` in the environment!')
 
+        self.datastore = None
         self.load_extension('jishaku')
 
-        for path in _LIB_EXTS.iterdir():
-            if path.is_dir():
-                name = path.name
+        failed = succeeded = 0
 
-            elif path.name.count('.') > 1:
-                print(f'Skipping: {path.name}')
-                continue
-
-            else:
-                name, *_ = path.name.rpartition('.')
-
+        for name in self._peek_extensions():
             try:
                 self.load_extension(f'src.cogs.{name}')
-            except Exception:
-                traceback.print_exc()
+            except commands.ExtensionFailed as error:
+                logger.critical(error)
+                self.dispatch('error', error)
+                failed += 1
+            else:
+                succeeded += 1
 
-    def add_cog(self, klass, *args, **kwargs):
-        super().add_cog(klass, *args, **kwargs)
-        getattr(klass, f'_{type(klass).__name__}__cog_add', (lambda: None))()
+        logger.info(
+            f'Loaded {succeeded + failed} cogs ({succeeded} succeeded and {failed} failed)'
+        )
+
+    def _peek_extensions(self) -> Tuple[str]:
+        """Collect the extensions to load and shuffle the list for priority"""
+        raw = [
+            (path.name if path.is_dir() else path.name.rsplit('.').pop(0))
+            for path in _LIB_EXTS.iterdir()
+            if path.is_dir() or (len(path.name) >= 4 and path.name.count('.') == 1)
+        ]
+
+        missing = tuple(
+            extension
+            for extension in PRIORITISED_EXTENSIONS
+            if extension.name not in raw and extension.critical
+        )
+
+        if missing:
+            raise RuntimeError(f'Critical extensions were not present! {missing!r}')
+        else:
+            extensions = [
+                (
+                    extension.name
+                    if extension.name in raw
+                    else logger.warn(f'Extension was not found! {extension!r}')
+                )
+                for extension in PRIORITISED_EXTENSIONS
+            ]
+            raw = [name for name in raw if name not in extensions]
+            extensions += raw
+
+        return tuple(extensions)
+
+    def add_cog(self, cog):
+        self._cog_counter += 1
+        super().add_cog(cog)
+        getattr(cog, f'_{type(cog).__name__}__cog_add', (lambda: None))()
+
+    def load_extension(self, name):
+        super().load_extension(name)
+        logger.info(f'Loaded extension: {name!r}')
+
+
+    async def on_command_error(self, ctx, error):
+        await ctx.send(error)
 
     async def on_connect(self):
-        print('Connect...', end='')
+        logger.info('Connected!')
 
     async def on_ready(self):
         self._guild = self.get_guild(_GUILD_SNOWFLAKE)
-        print('ready!')
+        logger.info('Ready!')
