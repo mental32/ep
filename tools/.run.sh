51bd962a63c0d49c6a35e583e9c0f605378ddb72
mental <m3nta1@yahoo.com>
2019-08-11 01:01:35 +0100

    Fix indentation

--- tools/.run.sh
+++ tools/.run.sh
@@ -2,8 +2,8 @@
 
 if python -c "import sys; sys.exit(not hasattr(sys, \"real_prefix\"))"; then
     echo "info: venv is active."
-	python -Bm src
+    python -Bm src
 else
     echo "info: venv not activated using \`pipenv\`"
-	pipenv run python -Bm src
+    pipenv run python -Bm src
 fi
