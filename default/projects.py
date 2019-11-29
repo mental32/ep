7e618eb88d1ed40173f5fae4f9b2b5083eaf342d
mental <m3nta1@yahoo.com>
2019-11-29 10:04:32 +0000

    Use Cog.formatted_regex in Projects Cog

--- default/projects.py
+++ default/projects.py
@@ -1,33 +1,32 @@
-import asyncio
-import sys
-from urllib.parse import urlparse
-from typing import List
+from asyncio import sleep
+from re import Match
+from typing import List, Dict, Any
 
 from discord import Message, Embed
 
 from ep import Cog
 
-_VALID_NETLOCS: List[str] = [
-    "github.com",
-]
+
+def _resolve_hosts(client) -> Dict[str, Any]:
+    return {"hosts": "|".join(client.config["default"].get("valid_vcs", []))}
 
 
 @Cog.export
 class Projects(Cog):
-    @Cog.event(tp="on_message", message_channel_id=633623473473847308, message_author_bot=False)
-    async def parse_message(self, message: Message) -> None:
+    _default_kwargs = {
+        "formatter": _resolve_hosts,
+        "filter_": (lambda match: isinstance(match, Match)),
+        "pattern": r"https?://($hosts)(?:.{0,200})",
+        "message_channel_id": 633623473473847308,
+        "message_author_bot": False,
+    }
+
+    @Cog.formatted_regex(**_default_kwargs)
+    async def filter_project_message(self, message: Message) -> None:
         """Handle a :class:`discord.Message` sent in the projects channel."""
-
-        url = await self.loop.run_in_executor(None, urlparse, message.content)
-
-        if not url.scheme:
-            await message.channel.send(f"{message.author.mention} That url didn't look quite right.", delete_after=3.0)
-            await asyncio.sleep(1)
-            return await message.delete()
-
-        if url.netloc not in _VALID_NETLOCS:
-            await message.delete()
-            await message.channel.send(f'"{message.author.mention} That doesn\'t look like a valid vcs link', delete_after=3.0)
-            await asyncio.sleep(1)
-
-        await message.channel.send(f"{message.author.mention} - <{message.content}>")
+        await message.channel.send(
+            f"{message.author.mention}, That doesn't look like a valid vcs link.",
+            delete_after=3.0,
+        )
+        await sleep(1.5)
+        await message.delete()
