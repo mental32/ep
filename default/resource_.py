73a5b3c069885877f3b3806fc369b2e7ab8d69fc
mental <m3nta1@yahoo.com>
2019-12-10 12:12:37 +0000

    Add file size limit

--- default/resource_.py
+++ default/resource_.py
@@ -3,7 +3,7 @@ from asyncio import Task, gather, Event, sleep
 from pathlib import Path
 from shutil import rmtree
 from tempfile import mkdtemp
-from typing import Set
+from typing import Set, List
 
 from discord import Message, File, HTTPException
 from ep import Cog
@@ -24,18 +24,35 @@ class Resource(Cog):
 
         self._paths = paths = self.config["default"]["resource"]["paths"]
         self._output_channel = self.config["default"]["resource"]["channel_id"]
+        self._file_limit = self.config["default"]["resource"].get("file_limit", 0x700000)
 
         self.client.schedule_task(self._hook_index_paths(paths))
+
+    @property
+    def file_limit(self) -> int:
+        return self._file_limit
 
     def cog_unload(self) -> None:
         for path in self._temporary_paths:
             rmtree(path)
 
     async def _hook(self, path: Path) -> None:
-        for sub in path.iterdir():
-            if sub.is_file() and sub.name.endswith(".pdf"):
-                self._filepath_cache[sub.name] = sub
+        n_large = 0
+        path = path.resolve().absolute()
+
+        def is_pdf_file(path: Path) -> bool:
+            return path.is_file() and path.name.endswith(".pdf")
+
+        for sub in filter(is_pdf_file, path.iterdir()):
+            if sub.stat().st_size <= self.file_limit:
+                self._filepath_cache[sub.name] = sub.resolve().absolute()
+            else:
+                n_large += 1
+
             await sleep(0)
+
+        if n_large:
+            self.logger.warn("Unable to register %s large files.", n_large)
 
     async def _clone_and_hook(self, url: str) -> None:
         dst = mkdtemp()
@@ -44,12 +61,12 @@ class Resource(Cog):
         self._temporary_paths.add(path)
         await self._hook(path)
 
-    async def _hook_index_paths(self) -> None:
+    async def _hook_index_paths(self, paths: List[str]) -> None:
         tasks: Set[Task] = set()
 
         current_directory = Path(__file__).parent.resolve().absolute()
 
-        for entry in self._paths:
+        for entry in paths:
             if any(
                 entry.startswith(protocol)
                 for protocol in ("http://", "https://", "git://")
